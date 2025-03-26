826f46b71954f9dccf011bf831a7f63c88115820
guimmie <pguimera@gmail.com>
2025-03-26 13:06:56 +0100

    Create joke object with its arrays

---
+++ main.js
@@ -0,0 +1,21 @@
+// Prints a "joke" with the next parts:
+// "- Why did the + {noun(1)} + {verb} + {noun(2)}?"
+// "- Because the {noun(2)} is {adjective}! + {emoji x3}"
+// There are 4 arrays within a main object "joke": nouns, verbs, adjectives and emojis
+// Arrays made with help from: https://randomwordgenerator.com/
+
+const joke = {
+  nouns: [
+    'fish', 'cat', 'dog', 'Brad Pitt', 'road', 'cloud', 'earth', 'highway', 'climate', 'cancer', 'response', 'hotel', 'poetry', 'depth', 'cigarette', 'satisfaction', 'potato', 'politics', 'thought', 'region', 'database', 'child', 'mom', 'hospital', 'argument', 'office', 'guitar', 'story', 'candidate', 'king', 'engine', 'membership', 'inspector', 'salad', 'analysis', 'location', 'oven', 'chemistry', 'airport', 'employment', 'art', 'control', 'association', 'city', 'property', 'version', 'growth', 'topic', 'sympathy', 'role', 'village', 'moment', 'debt', 'history', 'secretary', 'meaning', 'basket'
+  ],
+  verbs: [
+    'omit', 'substitute', 'respond', 'picture', 'allege', 'print', 'effect', 'pay', 'name', 'bid', 'consume', 'undertake', 'entail', 'press', 'replace', 'boast', 'disclose', 'manage', 'accuse', 'coincide'
+  ],
+  adjectives: [
+    'classy', 'overwrought', 'round', 'annoying', 'dynamic', 'skillful', 'electrical', 'new', 'well-groomed', 'itchy', 'glib', 'rambunctious', 'alert', 'vague', 'ripe', 'sexual', 'filthy', 'confused', 'unbiased', 'mute', 'unusual', 'material', 'unwieldy', 'futuristic', 'divergent', 'financial', 'didactic', 'stereotyped', 'faulty', 'sordid'
+  ],
+  emojis: ['🥳', '😭', '🤣', '🙈', '😂', '😲', '🤯']
+}
+
+
+
